ffd3a311c2d74775256d9630147562dddfe87418
caspersimon <165684406+caspersimon@users.noreply.github.com>
2024-12-13 12:32:18 +0100

    Quartz sync: Dec 13, 2024, 12:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Youth Media Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "en-UK",
+    baseUrl: "caspersimon.github.io/youth-media",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
